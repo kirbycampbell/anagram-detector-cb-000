681481fc63afd473a49b4dda066c8aa3c6b00bef
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-07 20:28:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,20 @@
 def Anagram
-
+attr_accessor :word
 
   def initialize(word)
-
+    @word = word
   end
 
+  def match(anagram_candidates)
+    matcher = word.split("")
 
+    anagram_candidates.find do |option|
+      option_array = option.split("")
+        if option_array == matcher
+          option
+        end
+      end
+  end
 
 end
+  
